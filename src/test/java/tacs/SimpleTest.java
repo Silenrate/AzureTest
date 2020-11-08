24d1c4066f2159721ea64c15a8717281d94d31f8
Silenrate <daniwalteros596@gmail.com>
2020-11-08 14:51:43 -0500

    New Test

--- src/test/java/tacs/SimpleTest.java
+++ src/test/java/tacs/SimpleTest.java
@@ -2,6 +2,10 @@ package tacs;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class SimpleTest {
@@ -9,5 +13,11 @@ public class SimpleTest {
     @Test
     public void shouldAssert() {
         assertTrue(true);
+    }
+
+    @Test
+    public void shouldEmpty() {
+        List<Integer> integers = new ArrayList<>();
+        assertEquals(0, integers.size());
     }
 }
