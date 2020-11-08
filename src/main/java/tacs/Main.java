8c24bf5d219c98377b8b4f3a600303cb4c5dbad6
Silenrate <daniwalteros596@gmail.com>
2020-11-08 14:15:51 -0500

    Basic Structure

---
+++ src/main/java/tacs/Main.java
@@ -0,0 +1,7 @@
+package tacs;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+}
---
+++ src/test/java/tacs/SimpleTest.java
@@ -0,0 +1,13 @@
+package tacs;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void shouldAssert() {
+        assertTrue(true);
+    }
+}
